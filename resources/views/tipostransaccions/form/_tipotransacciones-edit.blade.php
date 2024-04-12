b54b1ebb37779e5e6189b27b59bbcc09aac164db
Johban Clavijo <johbancgmail.com>
2024-04-12 11:47:05 -0500

    Act

--- resources/views/tipostransaccions/form/_tipotransacciones-edit.blade.php
+++ resources/views/tipostransaccions/form/_tipotransacciones-edit.blade.php
@@ -7,7 +7,7 @@
 	<div class="row">
 
 		<x-adminlte-input name="nombre" label="Nombre" placeholder="Nombre" fgroup-class="col-md-12"
-		type="text" class="{{ $errors->has('nombre') ? 'is-invalid' : '' }}" value="{{ old('nombre', isset($TipoTransaccion) ? $TipoTransaccion->nombre : '') }}" id="input-nombre" autofocus/>
+		type="text" class="{{ $errors->has('nombre') ? 'is-invalid' : '' }}" value="{{ old('nombre', isset($TipoTransaccion) ? $TipoTransaccion->nombre : '') }}" id="input-nombre"/>
 		@if ($errors->has('nombre'))
 		<div class="invalid-feedback">{{ $errors->first('nombre') }}</div>
 		@endif
